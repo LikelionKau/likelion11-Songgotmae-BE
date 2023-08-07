305d60474e136a838513879ae7b1fdc815b91128
egoing <egoing@gmail.com>
2023-08-07 20:54:08 +0900

    please pass me

--- src/main/java/likelion/underdog/songgotmae/web/PostController.java
+++ src/main/java/likelion/underdog/songgotmae/web/PostController.java
@@ -1,6 +1,5 @@
 package likelion.underdog.songgotmae.web;
 
-import likelion.underdog.songgotmae.domain.post.Post;
 import likelion.underdog.songgotmae.web.dto.CommonResponseDto;
 import likelion.underdog.songgotmae.web.dto.PostDto;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +17,8 @@ public class PostController {
     @PostMapping("/write")
     public CommonResponseDto createPost(PostDto postDto) {
 
-        Post post = new Post();
-        post.setUserId(postDto.getUserId());
-
-        CommonResponseDto responseDto = new CommonResponseDto();
-        responseDto.setId(post.getId());
+        CommonResponseDto responseDto = postService.newPost(postDto);
         responseDto.setResponse("게시글이 작성되었습니다.");
-
 
         return responseDto; // 컨트롤러에서는 dto를 반환하고
     }
