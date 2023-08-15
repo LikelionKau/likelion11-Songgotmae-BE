e1e9e6dd33908f142ba9711e96b702b8b14480da
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-08-15 13:18:29 +0900

    fix : add agreementDAO

---
+++ src/main/java/likelion/underdog/songgotmae/domain/agreement/AgreementRepository.java
@@ -0,0 +1,6 @@
+package likelion.underdog.songgotmae.domain.agreement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+}
