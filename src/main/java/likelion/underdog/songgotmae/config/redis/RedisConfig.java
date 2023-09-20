6c1ff8321c88e4b85fceabac6a05d5c5c2eb846b
egoing <egoing@gmail.com>
2023-09-20 23:20:05 +0900

    from Patrick to Vettel

--- src/main/java/likelion/underdog/songgotmae/config/redis/RedisConfig.java
+++ src/main/java/likelion/underdog/songgotmae/config/redis/RedisConfig.java
@@ -1,37 +1,37 @@
-package likelion.underdog.songgotmae.config.redis;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-@Configuration
-@EnableRedisRepositories
-public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        /*
-        * redisConnectionFactory : application.yml에 있는 host와 port 번호를 이용하여 redis와 spring을 연결
-        * */
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
-
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-        /*
-        * redisTemplate : RedisConnection을 통해 넘어온 byte값을 객체 직렬화해줌.
-        * */
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory((redisConnectionFactory()));
-        return redisTemplate;
-    }
-}
+//package likelion.underdog.songgotmae.config.redis;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+//
+//@Configuration
+//@EnableRedisRepositories
+//public class RedisConfig {
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        /*
+//        * redisConnectionFactory : application.yml에 있는 host와 port 번호를 이용하여 redis와 spring을 연결
+//        * */
+//        return new LettuceConnectionFactory(redisHost, redisPort);
+//    }
+//
+//    @Bean
+//    public RedisTemplate<?, ?> redisTemplate() {
+//        /*
+//        * redisTemplate : RedisConnection을 통해 넘어온 byte값을 객체 직렬화해줌.
+//        * */
+//        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory((redisConnectionFactory()));
+//        return redisTemplate;
+//    }
+//}
