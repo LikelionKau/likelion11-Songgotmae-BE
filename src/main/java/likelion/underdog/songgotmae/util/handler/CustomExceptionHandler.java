f367b1dc05d7841eb3aa272156b77ffc605821bc
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-13 13:09:34 +0900

    refac : responseDto spec - set data null

--- src/main/java/likelion/underdog/songgotmae/util/handler/CustomExceptionHandler.java
+++ src/main/java/likelion/underdog/songgotmae/util/handler/CustomExceptionHandler.java
@@ -1,14 +1,10 @@
 package likelion.underdog.songgotmae.util.handler;
 
 import com.auth0.jwt.exceptions.SignatureVerificationException;
-<<<<<<< HEAD
-import likelion.underdog.songgotmae.util.exception.*;
-=======
 import likelion.underdog.songgotmae.util.exception.CustomForbiddenException;
 import likelion.underdog.songgotmae.util.exception.CustomNotFoundException;
 import likelion.underdog.songgotmae.util.exception.CustomValidationException;
 import likelion.underdog.songgotmae.util.exception.MemberAlreadyExistException;
->>>>>>> UD-55
 import likelion.underdog.songgotmae.web.dto.CommonResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,32 +19,25 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomNotFoundException.class)
     public ResponseEntity<?> notFoundExcpetion(CustomNotFoundException e) {
         log.error(e.getMessage());
-        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage()/*, e*/), HttpStatus.NOT_FOUND); // 404
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), null), HttpStatus.NOT_FOUND); // 404
     }
 
     @ExceptionHandler(MemberAlreadyExistException.class)
     public ResponseEntity<?> memberAlreadyExistExcpetion(MemberAlreadyExistException e) {
         log.error(e.getMessage());
-        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage()/*, e*/), HttpStatus.BAD_REQUEST); // 400
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST); // 400
     }
 
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<?> invalidRequestException(CustomValidationException e) {
         log.error(e.getMessage());
-        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage()/*, e*/), HttpStatus.BAD_REQUEST); // 400
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST); // 400
     }
 
     @ExceptionHandler(CustomForbiddenException.class)
     public ResponseEntity<?> apiException(CustomForbiddenException e) {
         log.error(e.getMessage());
-        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage()/*, e*/), HttpStatus.FORBIDDEN); // 403
-    }
-/*
-* 의도대로 동작 x
-* */
-    @ExceptionHandler(SignatureVerificationException.class)
-    public ResponseEntity<?> invalidTokenException(InvalidTokenException e) {
-        log.error(e.getMessage());
-        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage()), HttpStatus.BAD_REQUEST);
+        // code는 성공하면 1, 실패하면 -1이므로 -1 반환 && data는 줄 게 없으므로 null
+        return new ResponseEntity<>(new CommonResponseDto<>(-1, e.getMessage(), null), HttpStatus.FORBIDDEN); // 403
     }
 }
