407a14f0177008f83cd5b1cb6c44a6725e1381e7
ghkdwp018 <99382672+ghkdwp018@users.noreply.github.com>
2023-11-13 19:30:45 +0900

    hotfix-cors

--- src/main/java/likelion/underdog/songgotmae/util/constant/CORS_PATTERNS.java
+++ src/main/java/likelion/underdog/songgotmae/util/constant/CORS_PATTERNS.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public class CORS_PATTERNS {
     public static final List<String> ALLOW_ORIGINS = Arrays.asList(
-//            "http://127.0.0.1:5500",
-//            "http://songgotmae.s3-website.ap-northeast-2.amazonaws.com",
-//            "http://127.0.0.1:63342"
-            "*"
+//            "*"
+            "http://songgotmae.s3-website.ap-northeast-2.amazonaws.com",
+            "http://127.0.0.1:5500",
+            "http://127.0.0.1:63342",
+            "118.235.13.99:63342"
             );
     public static final List<String> ALLOW_METHODS = Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH");
     public static final List<String> ALLOW_HEADERS = Arrays.asList("*");
